5747628022ea89bf77a7d1a93266186171a0fdde
Jay <hawkinsjr27@gmail.com>
2025-10-29 17:47:49 -0400

    working usb read/write

--- gui/src/offboard.py
+++ gui/src/offboard.py
@@ -1,32 +1,36 @@
 """Manages offboard (USB) configuration loading"""
 import fs
 import fs.base
+import os
 from utils import config_dir, root_dir
 
 SCAN_FOR_DIR: str = "multifx"
 
 # Scans for a test directory under gui root when true
-MOCK = True
+MOCK = False
 
-DEV_DIR = "/dev"
+USB_DIR = f"/run/media/{os.getlogin()}"
 if MOCK:
-    DEV_DIR = f"{root_dir}/.mockdev"
+    USB_DIR = f"{root_dir}/.mockdev"
 
 
 def scan_devices() -> fs.base.FS | None:
     """
-    Scan for /dev/sd*/ for possible USB drives, then returns the first
-    directory that contains SCAN_FOR_DIR
+    Scan for media drives, then returns the first directory that contains
+    SCAN_FOR_DIR
     """
-    dev_fs = fs.open_fs(DEV_DIR)
-    for dir in dev_fs.filterdir("", dirs=["sd*"]):
-        print(f"Checking {DEV_DIR}/{dir.name} for {SCAN_FOR_DIR}")
+    dev_fs = fs.open_fs(USB_DIR)
+    for dir in dev_fs.scandir(""):
+        # Skip files
+        if not dir.is_dir:
+            continue
+        print(f"Checking {USB_DIR}/{dir.name} for {SCAN_FOR_DIR}")
         for final_dir in dev_fs.filterdir(dir.name, dirs=[SCAN_FOR_DIR]):
             if not final_dir.is_dir:
                 break
             print(f"{SCAN_FOR_DIR} found in {dir.name}!")
-            return fs.open_fs(f"{DEV_DIR}/{dir.name}/{SCAN_FOR_DIR}")
-        print(f"{SCAN_FOR_DIR} directory not found in {DEV_DIR}/{dir.name}")
+            return fs.open_fs(f"{USB_DIR}/{dir.name}/{SCAN_FOR_DIR}")
+        print(f"{SCAN_FOR_DIR} directory not found in {USB_DIR}/{dir.name}")
     return None
 
 
