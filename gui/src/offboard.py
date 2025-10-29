118dbb052a35fcf8836ec36811cc8a1690b887da
Jay <hawkinsjr27@gmail.com>
2025-10-29 13:54:43 -0400

    successful testing with mock path

--- gui/src/offboard.py
+++ gui/src/offboard.py
@@ -1,22 +1,32 @@
 """Manages offboard (USB) configuration loading"""
-from fs import open_fs
-import re
+import fs
+import fs.base
+from utils import config_dir, root_dir
 
 SCAN_FOR_DIR: str = "multifx"
 
+# Scans for a test directory under gui root when true
+MOCK = True
 
-def scan_devices() -> str | None:
+DEV_DIR = "/dev"
+if MOCK:
+    DEV_DIR = f"{root_dir}/.mockdev"
+
+
+def scan_devices() -> fs.base.FS | None:
     """
     Scan for /dev/sd*/ for possible USB drives, then returns the first
     directory that contains SCAN_FOR_DIR
     """
-    dev_fs = open_fs("/dev/")
-    reg = re.compile("sd*")
-    for dir in filter(lambda d: reg.match(str(d)), dev_fs.scandir("")):
-        print(f"Checking /dev/{dir} for {SCAN_FOR_DIR}")
-        if SCAN_FOR_DIR in dev_fs.scandir(dir):
-            print(f"{SCAN_FOR_DIR} found in {dir}!")
-            return f"{dir}/{SCAN_FOR_DIR}"
+    dev_fs = fs.open_fs(DEV_DIR)
+    for dir in dev_fs.filterdir("", dirs=["sd*"]):
+        print(f"Checking {DEV_DIR}/{dir.name} for {SCAN_FOR_DIR}")
+        for final_dir in dev_fs.filterdir(dir.name, dirs=[SCAN_FOR_DIR]):
+            if not final_dir.is_dir:
+                break
+            print(f"{SCAN_FOR_DIR} found in {dir.name}!")
+            return fs.open_fs(f"{DEV_DIR}/{dir.name}/{SCAN_FOR_DIR}")
+        print(f"{SCAN_FOR_DIR} directory not found in {DEV_DIR}/{dir.name}")
     return None
 
 
@@ -24,14 +34,25 @@ def try_load() -> bool:
     """
     Attempts to load data from a USB, returns True if successful
     """
-    extcfgdir = scan_devices()
-    if not extcfgdir:
+    extcfg_fs = scan_devices()
+    if not extcfg_fs:
         return False
     # Copy to on-board config directory
+    incfg_fs = fs.open_fs(config_dir)
+    fs.copy.copy_dir(extcfg_fs, "", incfg_fs, "")
+
+    return True
 
 
 def try_save() -> bool:
     """
     Attempts to write on-board data to a USB, returns True if successful
     """
-    extcfgdir = scan_devices()
+    extcfg_fs = scan_devices()
+    if not extcfg_fs:
+        return False
+    # Copy from config to off-board drive
+    incfg_fs = fs.open_fs(config_dir)
+    fs.copy.copy_dir(incfg_fs, "", extcfg_fs, "")
+
+    return True
