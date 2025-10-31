8384cb3412eebdb49b5d716535cbacb3b5981ebd
Jay <hawkinsjr27@gmail.com>
2025-10-31 16:27:22 -0400

    added fallback to offboard

--- gui/src/offboard.py
+++ gui/src/offboard.py
@@ -9,9 +9,10 @@ SCAN_FOR_DIR: str = "multifx"
 # Scans for a test directory under gui root when true
 MOCK = False
 
-USB_DIR = f"/run/media/{os.getlogin()}"
+# USB directory and fallbacks
+USB_DIRS = [f"/run/media/{os.getlogin()}", f"/media/{os.getlogin()}"]
 if MOCK:
-    USB_DIR = f"{root_dir}/.mockdev"
+    USB_DIRS = [f"{root_dir}/.mockdev"]
 
 
 def scan_devices() -> fs.base.FS | None:
@@ -19,19 +20,23 @@ def scan_devices() -> fs.base.FS | None:
     Scan for media drives, then returns the first directory that contains
     SCAN_FOR_DIR
     """
-    dev_fs = fs.open_fs(USB_DIR)
-    for dir in dev_fs.scandir(""):
-        # Skip files
-        if not dir.is_dir:
+    for USB_DIR in USB_DIRS:
+        try:
+            dev_fs = fs.open_fs(USB_DIR)
+            for dir in dev_fs.scandir(""):
+                # Skip files
+                if not dir.is_dir:
+                    continue
+                print(f"Checking {USB_DIR}/{dir.name} for {SCAN_FOR_DIR}")
+                for final_dir in dev_fs.filterdir(dir.name, dirs=[SCAN_FOR_DIR]):
+                    if not final_dir.is_dir:
+                        break
+                    print(f"{SCAN_FOR_DIR} found in {dir.name}!")
+                    return fs.open_fs(f"{USB_DIR}/{dir.name}/{SCAN_FOR_DIR}")
+                print(f"{SCAN_FOR_DIR} directory not found in {USB_DIR}/{dir.name}")
+            return None
+        except fs.errors.CreateFailed:
             continue
-        print(f"Checking {USB_DIR}/{dir.name} for {SCAN_FOR_DIR}")
-        for final_dir in dev_fs.filterdir(dir.name, dirs=[SCAN_FOR_DIR]):
-            if not final_dir.is_dir:
-                break
-            print(f"{SCAN_FOR_DIR} found in {dir.name}!")
-            return fs.open_fs(f"{USB_DIR}/{dir.name}/{SCAN_FOR_DIR}")
-        print(f"{SCAN_FOR_DIR} directory not found in {USB_DIR}/{dir.name}")
-    return None
 
 
 def try_load() -> bool:
